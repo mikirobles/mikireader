5f0e1232cdeb1afe00d5255e6c7c044395c5eef8
Micael Robles <micael.robles@gmail.com>
2017-07-20 14:10:16 -0300

    Updates

---
+++ js/all.js
@@ -0,0 +1,51 @@
+var Book;
+
+function handleFileSelect(evt) {
+    document.querySelectorAll("#area div").forEach(div => {
+        div.remove();
+    })
+    evt.stopPropagation();
+    evt.preventDefault();
+    var files = evt.dataTransfer.files;
+    var path = (window.URL || window.webkitURL).createObjectURL(files[0]);
+    XMLRequest(path, readBook);
+}
+
+function XMLRequest(path, cb) {
+    var xhr = new XMLHttpRequest();
+    xhr.open('GET', path, true);
+    xhr.responseType = 'arraybuffer';
+    xhr.onload = function (e) {
+        if (this.status == 200) {
+            cb(this.response);
+        }
+    };
+    xhr.send();
+}
+
+function readBook(blob) {
+    Book = ePub(blob);
+    Book.renderTo("area");
+    $("#drop_zone").css("display", "none");
+    $(".turnPage").css("display", "block");
+    $("#area").addClass("margin-top-auto");
+}
+
+function handleDragOver(evt) {
+    evt.stopPropagation();
+    evt.preventDefault();
+    evt.dataTransfer.dropEffect = 'copy'; // Explicitly show this is a copy.
+}
+
+function prevPage() {
+    Book.prevPage();
+}
+
+function nextPage() {
+    Book.nextPage();
+}
+
+var dropZone = document.getElementById('drop_zone');
+dropZone.addEventListener('dragover', handleDragOver, false);
+dropZone.addEventListener('drop', handleFileSelect, false);
+//# sourceMappingURL=all.js.map
